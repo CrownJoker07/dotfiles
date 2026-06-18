f5f9b36703abe50243d3797c3285495cda6ffb01
ZhuangZewei <3462294546@qq.com>
2026-06-18 14:44:04 +0800

    feat(neovim):添加局部格式化

--- config/base/nvim/lua/plugins/format.lua
+++ config/base/nvim/lua/plugins/format.lua
@@ -18,6 +18,23 @@ return {
         end,
         desc = "Format file",
       },
+      {
+        "<leader>cf",
+        function()
+          local start_pos = vim.fn.getpos("'<")
+          local end_pos = vim.fn.getpos("'>")
+          require("conform").format({
+            async = true,
+            lsp_format = "fallback",
+            range = {
+              start = { start_pos[2], start_pos[3] },
+              ["end"] = { end_pos[2], end_pos[3] },
+            },
+          })
+        end,
+        mode = "v",
+        desc = "Format selection",
+      },
     },
     opts = {
       formatters_by_ft = {
