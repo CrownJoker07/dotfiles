04ceca4975dd3cd6f0a8e4aa9a3a5a1bbf8adfc8
ZeweiZhuang <3462294546@qq.com>
2026-06-24 08:40:45 +0800

    feat(neovim):优化symbol显示，直接全屏展示

--- config/base/nvim/lua/plugins/ui.lua
+++ config/base/nvim/lua/plugins/ui.lua
@@ -69,11 +69,27 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
-    opts = {},
+    opts = {
+      modes = {
+        symbols = {
+          focus = true,
+          win = {
+            type = "float",
+            relative = "editor",
+            position = { 0, 0 },
+            size = { width = 1.0, height = 1.0 },
+            border = "none",
+          },
+          keys = {
+            ["<cr>"] = "jump_close",
+          },
+        },
+      },
+    },
     keys = {
       { "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Diagnostics" },
       { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Buffer diagnostics" },
-      { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<CR>", desc = "Symbols" },
+      { "<leader>cs", "<cmd>Trouble symbols toggle<CR>", desc = "Symbols" },
       { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<CR>", desc = "LSP definitions/references" },
       { "<leader>xL", "<cmd>Trouble loclist toggle<CR>", desc = "Location list" },
       { "<leader>xQ", "<cmd>Trouble qflist toggle<CR>", desc = "Quickfix list" },
