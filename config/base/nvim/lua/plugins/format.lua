4ab8aed0d0e108a3fa64c5247c9488d912206cde
ZhuangZewei <3462294546@qq.com>
2026-06-24 16:56:31 +0800

    feat(neovim)：优化格式化逻辑

--- config/base/nvim/lua/plugins/format.lua
+++ config/base/nvim/lua/plugins/format.lua
@@ -1,3 +1,61 @@
+local function format()
+  local conform = require("conform")
+  local mode = vim.api.nvim_get_mode().mode
+
+  if mode == "n" then
+    conform.format({
+      async = true,
+      lsp_format = "fallback",
+    })
+    return
+  end
+
+  if mode ~= "v" and mode ~= "V" then
+    vim.notify("Blockwise range formatting is not supported", vim.log.levels.WARN)
+    return
+  end
+
+  local bufnr = vim.api.nvim_get_current_buf()
+  local formatters = conform.list_formatters(bufnr)
+  local has_formatters = #formatters > 0
+  local all_support_range = has_formatters
+
+  for _, formatter in ipairs(formatters) do
+    local config = conform.get_formatter_config(formatter.name, bufnr)
+    if not config or not config.range_args then
+      all_support_range = false
+      break
+    end
+  end
+
+  if all_support_range then
+    conform.format({
+      async = true,
+      lsp_format = "never",
+    })
+    return
+  end
+
+  local clients = vim.lsp.get_clients({
+    bufnr = bufnr,
+    method = "textDocument/rangeFormatting",
+  })
+
+  if #clients > 0 then
+    vim.lsp.buf.format({
+      async = true,
+      bufnr = bufnr,
+      id = clients[1].id,
+    })
+    return
+  end
+
+  vim.notify(
+    "No formatter supports reliable range formatting for this buffer",
+    vim.log.levels.WARN
+  )
+end
+
 return {
   {
     "stevearc/conform.nvim",
@@ -7,30 +65,9 @@ return {
     keys = {
       {
         "<leader>cf",
-        function()
-          require("conform").format({
-            async = true,
-            lsp_format = "fallback",
-          })
-        end,
-        desc = "Format file",
-      },
-      {
-        "<leader>cf",
-        function()
-          local start_pos = vim.fn.getpos("'<")
-          local end_pos = vim.fn.getpos("'>")
-          require("conform").format({
-            async = true,
-            lsp_format = "fallback",
-            range = {
-              start = { start_pos[2], start_pos[3] },
-              ["end"] = { end_pos[2], end_pos[3] },
-            },
-          })
-        end,
-        mode = "v",
-        desc = "Format selection",
+        format,
+        mode = { "n", "v" },
+        desc = "Format file or selection",
       },
     },
     opts = {
