1260fb195e2c79c06ca2a09be1786b8655b46583
ZhuangZewei <3462294546@qq.com>
2026-06-25 13:52:16 +0800

    feat(neovim):优化 neoscroll.nvim

--- config/base/nvim/lua/plugins/ui.lua
+++ config/base/nvim/lua/plugins/ui.lua
@@ -46,16 +46,20 @@ return {
   -- Smooth scrolling
   {
     "karb94/neoscroll.nvim",
+    opts = {
+      mappings = {},
+      easing = "quadratic",
+    },
     keys = {
-      { "<C-u>", function() require("neoscroll").ctrl_u({ duration = 100, easing = "quadratic" }) end, mode = { "n", "v", "x" } },
-      { "<C-d>", function() require("neoscroll").ctrl_d({ duration = 100, easing = "quadratic" }) end, mode = { "n", "v", "x" } },
-      { "<C-b>", function() require("neoscroll").ctrl_b({ duration = 200, easing = "quadratic" }) end, mode = { "n", "v", "x" } },
-      { "<C-f>", function() require("neoscroll").ctrl_f({ duration = 200, easing = "quadratic" }) end, mode = { "n", "v", "x" } },
-      { "<C-y>", function() require("neoscroll").scroll(-0.1, { move_cursor = false, duration = 50, easing = "quadratic" }) end, mode = { "n", "v", "x" } },
-      { "<C-e>", function() require("neoscroll").scroll(0.1, { move_cursor = false, duration = 50, easing = "quadratic" }) end, mode = { "n", "v", "x" } },
-      { "zt", function() require("neoscroll").zt({ half_win_duration = 100, easing = "quadratic" }) end, mode = { "n", "v", "x" } },
-      { "zz", function() require("neoscroll").zz({ half_win_duration = 100, easing = "quadratic" }) end, mode = { "n", "v", "x" } },
-      { "zb", function() require("neoscroll").zb({ half_win_duration = 100, easing = "quadratic" }) end, mode = { "n", "v", "x" } },
+      { "<C-u>", function() require("neoscroll").ctrl_u({ duration = 100 }) end, mode = { "n", "v", "x" } },
+      { "<C-d>", function() require("neoscroll").ctrl_d({ duration = 100 }) end, mode = { "n", "v", "x" } },
+      { "<C-b>", function() require("neoscroll").ctrl_b({ duration = 200 }) end, mode = { "n", "v", "x" } },
+      { "<C-f>", function() require("neoscroll").ctrl_f({ duration = 200 }) end, mode = { "n", "v", "x" } },
+      { "<C-y>", function() require("neoscroll").scroll(-0.1, { move_cursor = false, duration = 50 }) end, mode = { "n", "v", "x" } },
+      { "<C-e>", function() require("neoscroll").scroll(0.1, { move_cursor = false, duration = 50 }) end, mode = { "n", "v", "x" } },
+      { "zt", function() require("neoscroll").zt({ half_win_duration = 100 }) end, mode = { "n", "v", "x" } },
+      { "zz", function() require("neoscroll").zz({ half_win_duration = 100 }) end, mode = { "n", "v", "x" } },
+      { "zb", function() require("neoscroll").zb({ half_win_duration = 100 }) end, mode = { "n", "v", "x" } },
     },
   },
 
