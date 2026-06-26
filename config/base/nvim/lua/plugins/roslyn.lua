8591f5251173c630e0ab0be3aea436337be0f6c6
ZhuangZewei <3462294546@qq.com>
2026-06-26 13:34:34 +0800

    Extract Roslyn LSP settings

--- config/base/nvim/lua/plugins/roslyn.lua
+++ config/base/nvim/lua/plugins/roslyn.lua
@@ -1,3 +1,27 @@
+local settings = {
+  ["csharp|inlay_hints"] = {
+    csharp_enable_inlay_hints_for_implicit_object_creation = true,
+    csharp_enable_inlay_hints_for_implicit_variable_types = true,
+    csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+    csharp_enable_inlay_hints_for_types = true,
+    dotnet_enable_inlay_hints_for_indexer_parameters = true,
+    dotnet_enable_inlay_hints_for_literal_parameters = true,
+    dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+    dotnet_enable_inlay_hints_for_other_parameters = true,
+    dotnet_enable_inlay_hints_for_parameters = true,
+  },
+  ["csharp|code_lens"] = {
+    dotnet_enable_references_code_lens = true,
+  },
+  ["csharp|completion"] = {
+    dotnet_show_completion_items_from_unimported_namespaces = true,
+    dotnet_show_name_completion_suggestions = true,
+  },
+  ["csharp|formatting"] = {
+    dotnet_organize_imports_on_format = true,
+  },
+}
+
 return {
   {
     "seblyng/roslyn.nvim",
@@ -10,39 +34,15 @@ return {
       "saghen/blink.cmp",
     },
     opts = {
+      -- Keep solution discovery narrow by default; broad search can be expensive in large repos.
       broad_search = false,
       filewatching = "roslyn",
     },
     config = function(_, opts)
       vim.lsp.config("roslyn", {
-        cmd = {
-          "roslyn-language-server",
-          "--stdio",
-        },
+        cmd = { "roslyn-language-server", "--stdio" },
         capabilities = require("blink.cmp").get_lsp_capabilities(),
-        settings = {
-          ["csharp|inlay_hints"] = {
-            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-            csharp_enable_inlay_hints_for_implicit_variable_types = true,
-            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-            csharp_enable_inlay_hints_for_types = true,
-            dotnet_enable_inlay_hints_for_indexer_parameters = true,
-            dotnet_enable_inlay_hints_for_literal_parameters = true,
-            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-            dotnet_enable_inlay_hints_for_other_parameters = true,
-            dotnet_enable_inlay_hints_for_parameters = true,
-          },
-          ["csharp|code_lens"] = {
-            dotnet_enable_references_code_lens = true,
-          },
-          ["csharp|completion"] = {
-            dotnet_show_completion_items_from_unimported_namespaces = true,
-            dotnet_show_name_completion_suggestions = true,
-          },
-          ["csharp|formatting"] = {
-            dotnet_organize_imports_on_format = true,
-          },
-        },
+        settings = settings,
       })
 
       require("roslyn").setup(opts)
