80b861695bd0e0bf0b45fb2f592e1ad111517b72
ZhuangZewei <3462294546@qq.com>
2026-06-26 13:32:53 +0800

    Avoid Mason installs in headless Neovim

--- config/base/nvim/lua/plugins/mason-tool-installer.lua
+++ config/base/nvim/lua/plugins/mason-tool-installer.lua
@@ -1,3 +1,12 @@
+local tools = {
+  "csharpier",
+  "prettier",
+  "roslyn-language-server",
+  "shfmt",
+  "stylua",
+  "tree-sitter-cli",
+}
+
 return {
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
@@ -6,16 +15,15 @@ return {
     },
     opts = function()
       return {
+        -- Avoid automatic installs in headless checks; manual commands still work.
         run_on_start = #vim.api.nvim_list_uis() > 0,
         start_delay = 3000,
         debounce_hours = 5,
-        ensure_installed = {
-          "csharpier",
-          "prettier",
-          "roslyn-language-server",
-          "shfmt",
-          "stylua",
-          "tree-sitter-cli",
+        ensure_installed = tools,
+        integrations = {
+          ["mason-lspconfig"] = false,
+          ["mason-null-ls"] = false,
+          ["mason-nvim-dap"] = false,
         },
       }
     end,
