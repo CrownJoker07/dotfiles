3345605f937d84a6acfcfdfb34ac9d3d8f2677c3
ZhuangZewei <3462294546@qq.com>
2026-06-27 11:55:31 +0800

    feat(neovim):优化 lazy.lua配置

--- config/base/nvim/lua/config/lazy.lua
+++ config/base/nvim/lua/config/lazy.lua
@@ -2,7 +2,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local output = vim.fn.system({
+  local out = vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
@@ -12,7 +12,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 
   if vim.v.shell_error ~= 0 then
-    error("Failed to clone lazy.nvim:\n" .. output)
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
   end
 end
 
@@ -22,6 +28,14 @@ require("lazy").setup({
   spec = {
     { import = "plugins" },
   },
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+  install = {
+    colorscheme = { "catppuccin-mocha", "habamax" },
+  },
+  local_spec = false,
   checker = {
     enabled = false,
     notify = false,
