6bf6a601e729fc31685b703a90a33a2e625e3fe2
ZhuangZewei <3462294546@qq.com>
2026-06-29 10:44:50 +0800

    feat(neovim):优化 gitsigns 配置

--- config/base/nvim/lua/plugins/gitsigns.lua
+++ config/base/nvim/lua/plugins/gitsigns.lua
@@ -20,10 +20,21 @@ return {
 
         local function nav_hunk(direction, diff_key)
           if vim.wo.diff then
-            vim.cmd.normal({ diff_key, bang = true })
+            vim.cmd.normal({ vim.v.count1 .. diff_key, bang = true })
           else
             gitsigns.nav_hunk(direction)
           end
+        end
+
+        local function selected_range()
+          local first = vim.fn.line(".")
+          local last = vim.fn.line("v")
+
+          if first > last then
+            first, last = last, first
+          end
+
+          return { first, last }
         end
 
         gs_map("n", "]c", function()
@@ -36,16 +47,21 @@ return {
 
         gs_map("n", "<leader>gp", gitsigns.preview_hunk, "Preview git hunk")
         gs_map("n", "<leader>gb", gitsigns.blame_line, "Git blame line")
+        gs_map("n", "<leader>gB", gitsigns.toggle_current_line_blame, "Toggle git blame line")
+        gs_map("n", "<leader>gd", gitsigns.diffthis, "Git diff current file")
+        gs_map("n", "<leader>gq", gitsigns.setloclist, "Git hunks location list")
         gs_map("n", "<leader>gr", gitsigns.reset_hunk, "Reset git hunk")
         gs_map("n", "<leader>gs", gitsigns.stage_hunk, "Stage git hunk")
 
         gs_map("v", "<leader>gr", function()
-          gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          gitsigns.reset_hunk(selected_range())
         end, "Reset selected git hunk")
 
         gs_map("v", "<leader>gs", function()
-          gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          gitsigns.stage_hunk(selected_range())
         end, "Stage selected git hunk")
+
+        gs_map({ "o", "x" }, "ih", gitsigns.select_hunk, "Git hunk")
       end,
     },
   },
