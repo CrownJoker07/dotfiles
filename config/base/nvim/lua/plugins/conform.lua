576ea014b46f957750010907ddfd8915966cedf6
ZhuangZewei <3462294546@qq.com>
2026-07-10 18:32:35 +0800

    feat(neovim):优化代码格式化

--- config/base/nvim/lua/plugins/conform.lua
+++ config/base/nvim/lua/plugins/conform.lua
@@ -1,3 +1,12 @@
+local function editorconfig_root(bufnr)
+  local filename = vim.api.nvim_buf_get_name(bufnr)
+  if filename == "" then
+    return nil
+  end
+
+  return vim.fs.root(vim.fs.dirname(filename), ".editorconfig")
+end
+
 local function format()
   -- In visual mode, conform.nvim automatically uses the selected range. If a
   -- formatter does not support native range formatting, conform applies a
@@ -34,6 +43,28 @@ return {
         cs = { "csharpier" },
         sh = { "shfmt" },
         bash = { "shfmt" },
+      },
+      formatters = {
+        prettier = {
+          cwd = function(_, ctx)
+            return editorconfig_root(ctx.buf)
+          end,
+        },
+        stylua = {
+          cwd = function(_, ctx)
+            return editorconfig_root(ctx.buf)
+          end,
+        },
+        csharpier = {
+          cwd = function(_, ctx)
+            return editorconfig_root(ctx.buf)
+          end,
+        },
+        shfmt = {
+          cwd = function(_, ctx)
+            return editorconfig_root(ctx.buf)
+          end,
+        },
       },
     },
   },
