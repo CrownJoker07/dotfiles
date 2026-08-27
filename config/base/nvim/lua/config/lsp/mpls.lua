a91943be34da540b26e8b22a654d7addc2814c1e
ZhuangZewei <3462294546@qq.com>
2026-08-27 17:04:26 +0800

    feat(neovim):优化mpls逻辑

--- config/base/nvim/lua/config/lsp/mpls.lua
+++ config/base/nvim/lua/config/lsp/mpls.lua
@@ -15,35 +15,13 @@ return {
       ".marksman.toml",
       ".git",
     },
-    handlers = {
-      ["window/showDocument"] = function(err, params, ctx, config)
-        if params and params.uri and not params.uri:match("^%a[%w+.-]*:") then
-          params = vim.deepcopy(params)
-          params.uri = vim.uri_from_fname(params.uri)
-        end
-
-        return vim.lsp.handlers["window/showDocument"](err, params, ctx, config)
-      end,
-    },
     on_attach = function(client, bufnr)
-      vim.api.nvim_create_autocmd("BufEnter", {
-        buffer = bufnr,
-        callback = function(args)
-          client:notify("mpls/editorDidChangeFocus", {
-            uri = vim.uri_from_bufnr(args.buf),
-          })
-        end,
-        desc = "Notify mpls when a Markdown buffer gains focus",
-      })
-
-      vim.api.nvim_buf_create_user_command(bufnr, "MplsOpenPreview", function()
+      vim.keymap.set("n", "<leader>mp", function()
         client:exec_cmd({
           title = "Preview Markdown with mpls",
           command = "open-preview",
         })
-      end, { desc = "Preview Markdown with mpls" })
-
-      vim.keymap.set("n", "<leader>mp", "<cmd>MplsOpenPreview<cr>", {
+      end, {
         buffer = bufnr,
         desc = "Open Markdown preview",
       })
